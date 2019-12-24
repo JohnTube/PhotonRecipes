bd96c1be33f7ea74248d6a2eb25c6cbef229597c
Hamza Lazaar <hamza.lazaar@photonengine.com>
2019-12-24 13:48:53 +0100

    ADDED: interest groups extension methods

--- Assets/PolyTics/Photon/Recipes/ExtensionMethods.cs
+++ Assets/PolyTics/Photon/Recipes/ExtensionMethods.cs
@@ -233,5 +233,53 @@ namespace PolyTics.Photon.Recipes
         }
 
         #endregion
+
+        #region Interest Groups
+
+        internal static readonly byte[] emptyByteArray = new byte[0];
+
+        internal static readonly byte[] allByteValues = Enumerable.Range(1, 255).SelectMany(BitConverter.GetBytes).ToArray(); 
+
+        public static bool AddInterestGroup(this LoadBalancingClient client, byte group)
+        {
+            return client.InRoom && client.OpChangeGroups(null, new[] { group });
+        }
+
+        public static bool AddInterestGroups(this LoadBalancingClient client, byte[] groups)
+        {
+            return client.InRoom && client.OpChangeGroups(null, groups);
+        }
+
+        public static bool RemoveInterestGroup(this LoadBalancingClient client, byte group)
+        {
+            return client.InRoom && client.OpChangeGroups(new[] { group }, null);
+        }
+
+        public static bool RemoveInterestGroups(this LoadBalancingClient client, byte[] groups)
+        {
+            return client.InRoom && client.OpChangeGroups(groups, null);
+        }
+
+        public static bool AddAllExistingInterestGroups(this LoadBalancingClient client)
+        {
+            return client.InRoom && client.OpChangeGroups(null, emptyByteArray);
+        }
+
+        public static bool RemoveAllExistingInterestGroups(this LoadBalancingClient client)
+        {
+            return client.InRoom && client.OpChangeGroups(emptyByteArray, null);
+        }
+
+        public static bool AddAllPossibleInterestGroups(this LoadBalancingClient client)
+        {
+            return client.InRoom && client.OpChangeGroups(null, allByteValues);
+        }
+
+        public static bool RemoveAllPossibleInterestGroups(this LoadBalancingClient client)
+        {
+            return client.InRoom && client.OpChangeGroups(allByteValues, null);
+        }
+
+        #endregion
     }
 }
