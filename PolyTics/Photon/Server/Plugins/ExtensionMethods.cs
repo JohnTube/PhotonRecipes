36939e85af1744ec92b571935a922d1c8668cd3c
Hamza Lazaar <brice2k5@gmail.com>
2021-11-09 23:47:37 +0100

    ADDED: moar extension methods FIXED: obsolete CreateOneTimeTimer calls FIXED: one time timers need to be stopped CHANGED: namespace

--- PolyTics/Photon/Server/Plugins/ExtensionMethods.cs
+++ PolyTics/Photon/Server/Plugins/ExtensionMethods.cs
@@ -1,4 +1,4 @@
-namespace PolyTics.Photon.Server.Plugins
+namespace JohnTube.Photon.Server.Plugins
 {
     using System.Collections;
     using System.Collections.Generic;
@@ -42,12 +42,22 @@ namespace PolyTics.Photon.Server.Plugins
 
         public static bool SetActorNickname(this IPluginHost pluginHost, int actorNr, string nickname, bool broadcast)
         {
-            return pluginHost.SetActorProperty(actorNr, 255, nickname, broadcast);
+            return pluginHost.SetActorProperties(actorNr, new Hashtable {{ (byte)255, nickname}}, null, broadcast);
+        }
+        
+        public static bool SetActorNickname(this IPluginHost pluginHost, int actorNr, string nickname, string oldNickname, bool broadcast)
+        {
+            return pluginHost.SetActorProperties(actorNr, new Hashtable {{ (byte)255, nickname}}, new Hashtable {{ (byte)255, oldNickname}}, broadcast);
         }
 
-        public static bool SetActorProperty<T>(this IPluginHost pluginHost, int actorNr, byte propKey, T propValue, bool broadcast)
+        public static bool SetCustomActorProperty<T>(this IPluginHost pluginHost, int actorNr, string propKey, T propValue, bool broadcast)
         {
             return pluginHost.SetActorProperties(actorNr, new Hashtable {{propKey, propValue}}, null, broadcast);
+        }
+
+        public static bool SetCustomActorProperty<T>(this IPluginHost pluginHost, int actorNr, string propKey, T propValue, T expectedValue, bool broadcast)
+        {
+            return pluginHost.SetActorProperties(actorNr, new Hashtable {{propKey, propValue}}, new Hashtable {{propKey, expectedValue}}, broadcast);
         }
 
         public static bool SetRoomProperty<T>(this IPluginHost pluginHost, byte propKey, T propValue, bool broadcast)
@@ -68,6 +78,150 @@ namespace PolyTics.Photon.Server.Plugins
         public static bool SetCustomRoomProperty<T>(this IPluginHost pluginHost, string propKey, T propValue, T expectedValue, bool broadcast)
         {
             return pluginHost.SetRoomProperties(new Hashtable {{propKey, propValue}}, new Hashtable {{propKey, expectedValue}}, broadcast);
+        }
+
+        public static bool SetInitialRoomProperties(this ICreateGameCallInfo info, Hashtable properties, bool replaceAll = false)
+        {
+            if (info.IsProcessed || properties == null)
+            {
+                return false;
+            }
+            if (replaceAll || info.Request.GameProperties == null)
+            {
+                info.Request.GameProperties = properties;
+            }
+            else
+            {
+                foreach (object key in properties.Keys)
+                {
+                    info.Request.GameProperties[key] = properties[key];
+                }
+            }
+            return true;
+        }
+
+        public static bool SetInitialActorProperties(this ICreateGameCallInfo info, Hashtable properties, bool replaceAll = false)
+        {
+            if (info.IsProcessed || properties == null)
+            {
+                return false;
+            }
+            if (replaceAll || info.Request.ActorProperties == null)
+            {
+                info.Request.ActorProperties = properties;
+            }
+            else
+            {
+                foreach (object key in properties.Keys)
+                {
+                    info.Request.ActorProperties[key] = properties[key];
+                }
+            }
+            return true;
+        }
+
+        public static bool SetInitialActorProperties(this IBeforeJoinGameCallInfo info, Hashtable properties, bool replaceAll = false)
+        {
+            if (info.IsProcessed || properties == null)
+            {
+                return false;
+            }
+            if (replaceAll || info.Request.ActorProperties == null)
+            {
+                info.Request.ActorProperties = properties;
+            }
+            else
+            {
+                foreach (object key in properties.Keys)
+                {
+                    info.Request.ActorProperties[key] = properties[key];
+                }
+            }
+            return true;
+        }
+
+        public static bool SetInitialActorNickname(this ICreateGameCallInfo info, string nickname)
+        {
+            if (info.IsProcessed)
+            {
+                return false;
+            }
+            if (info.Request.ActorProperties == null)
+            {
+                info.Request.ActorProperties = new Hashtable();
+            }
+            info.Request.ActorProperties[(byte)255] = nickname;
+            return true;
+        }
+
+        public static bool SetInitialActorNickname(this IBeforeJoinGameCallInfo info, string nickname)
+        {
+            if (info.IsProcessed)
+            {
+                return false;
+            }
+            if (info.Request.ActorProperties == null)
+            {
+                info.Request.ActorProperties = new Hashtable();
+            }
+            info.Request.ActorProperties[(byte)255] = nickname;
+            return true;
+        }
+
+        public static bool SetInitialCustomActorProperty<T>(this ICreateGameCallInfo info, string propKey, T propValue)
+        {
+            if (info.IsProcessed)
+            {
+                return false;
+            }
+            if (info.Request.ActorProperties == null)
+            {
+                info.Request.ActorProperties = new Hashtable();
+            }
+            info.Request.ActorProperties[propKey] = propValue;
+            return true;
+        }
+
+        public static bool SetInitialCustomActorProperty<T>(this IBeforeJoinGameCallInfo info, string propKey, T propValue)
+        {
+            if (info.IsProcessed)
+            {
+                return false;
+            }
+            if (info.Request.ActorProperties == null)
+            {
+                info.Request.ActorProperties = new Hashtable();
+            }
+            info.Request.ActorProperties[propKey] = propValue;
+            return true;
+        }
+
+        public static bool SetInitialRoomProperty<T>(this ICreateGameCallInfo info, byte propKey, T propValue)
+        {
+            if (info.IsProcessed)
+            {
+                return false;
+            }
+            if (info.Request.GameProperties == null)
+            {
+                info.Request.GameProperties = new Hashtable();
+            }
+            info.Request.GameProperties[propKey] = propValue;
+            return true;
+        }
+
+        public static bool SetInitialCustomRoomProperty<T>(this ICreateGameCallInfo info, string propKey, T propValue)
+        {
+            if (info.IsProcessed)
+            {
+                return false;
+            }
+            if (info.Request.GameProperties == null)
+            {
+                info.Request.GameProperties = new Hashtable();
+            }
+            info.Request.GameProperties[propKey] = propValue;
+            return true;
         }
 
         #endregion
@@ -97,17 +251,41 @@ namespace PolyTics.Photon.Server.Plugins
         public static bool TryGetActorProperty<T>(this IPluginHost pluginHost, int actorNr, byte key, out T property)
         {
             property = default;
-            if (pluginHost.TryGetActorByNumber(actorNr, out IActor actor) && actor.Properties.TryGetValue(key, out object temp))
+            return pluginHost.TryGetActorByNumber(actorNr, out IActor actor) && actor.TryGetActorProperty(key, out property);
+        }
+
+        public static bool TryGetCustomActorProperty<T>(this IPluginHost pluginHost, int actorNr, string key, out T property)
+        {
+            property = default;
+            return pluginHost.TryGetActorByNumber(actorNr, out IActor actor) && actor.TryGetCustomActorProperty(key, out property);
+        }
+
+        public static bool TryGetActorProperty<T>(this IPluginHost pluginHost, string userId, byte key, out T property)
+        {
+            property = default;
+            return pluginHost.TryGetActorByUserId(userId, out IActor actor) && actor.TryGetActorProperty(key, out property);
+        }
+
+        public static bool TryGetCustomActorProperty<T>(this IPluginHost pluginHost, string userId, string key, out T property)
+        {
+            property = default;
+            return pluginHost.TryGetActorByUserId(userId, out IActor actor) && actor.TryGetCustomActorProperty(key, out property);
+        }
+
+        public static bool TryGetActorProperty<T>(this IActor actor, byte key, out T property)
+        {
+            property = default;
+            if (actor.Properties.TryGetValue(key, out object temp) && temp is T)
             {
                 property = (T)temp;
             }
             return false;
         }
 
-        public static bool TryGetCustomActorProperty<T>(this IPluginHost pluginHost, int actorNr, string key, out T property)
+        public static bool TryGetCustomActorProperty<T>(this IActor actor, string key, out T property)
         {
             property = default;
-            if (pluginHost.TryGetActorByNumber(actorNr, out IActor actor) && actor.Properties.TryGetValue(key, out object temp))
+            if (actor.Properties.TryGetValue(key, out object temp) && temp is T)
             {
                 property = (T)temp;
             }
@@ -152,20 +330,72 @@ namespace PolyTics.Photon.Server.Plugins
 
         public const int RemoveActorEventCode = 199;
         public const int RemoveActorTimerDelay = 200;
-
        
-        public static void RemoveActor(this IPluginHost pluginHost, int actorNr, string reason)
+        public static void RemoveActor(this IPluginHost pluginHost, ICallInfo info, int actorNr, string reason)
         {
             object eventData = reason;
             pluginHost.RaiseEvent(RemoveActorEventCode, eventData, new List<int> { actorNr });
-            pluginHost.CreateOneTimeTimer(() => pluginHost.RemoveActor(actorNr, reason), RemoveActorTimerDelay);
+            object timer = null;
+            timer = pluginHost.CreateOneTimeTimer(info, () =>
+            {
+                pluginHost.RemoveActor(actorNr, reason);
+                pluginHost.StopTimer(timer);
+            }, RemoveActorTimerDelay);
         }
 
-        public static void RemoveActor(this IPluginHost pluginHost, int actorNr, byte reasonCode, string reason)
+        public static void RemoveActor(this IPluginHost pluginHost, ICallInfo info, int actorNr, byte reasonCode, string reason)
         {
             object[] eventData = { reasonCode, reason };
             pluginHost.RaiseEvent(RemoveActorEventCode, eventData, new List<int> { actorNr });
-            pluginHost.CreateOneTimeTimer(() => pluginHost.RemoveActor(actorNr, reasonCode, reason), RemoveActorTimerDelay);
+            object timer = null;
+            timer = pluginHost.CreateOneTimeTimer(info, () =>
+            {
+                pluginHost.RemoveActor(actorNr, reasonCode, reason);
+                pluginHost.StopTimer(timer);
+            }, RemoveActorTimerDelay);
+        }
+
+        #endregion
+
+        #region Alter Operation Request
+
+        public static bool TrySetParameter<T>(this IOperationRequest request, byte parameterKeyCode, T parameterValue, bool mustExist = false)
+        {
+            if (request.Parameters == null)
+            {
+                return false;
+            }
+            if (!mustExist || request.Parameters.ContainsKey(parameterKeyCode))
+            {
+                request.Parameters[parameterKeyCode] = parameterValue;
+                return true;
+            }
+            return false;
+        }
+
+        public static bool TrySetParameter<T>(this ICallInfo info, byte parameterKeyCode, T parameterValue, bool mustExist = false)
+        {
+            return info.OperationRequest.TrySetParameter(parameterKeyCode, parameterValue, mustExist);
+        }
+
+        public static bool TryGetParameter<T>(this IOperationRequest request, byte parameterKeyCode, out T parameterValue)
+        {
+            parameterValue = default;
+            if (request.Parameters == null)
+            {
+                return false;
+            }
+            if (!request.Parameters.TryGetValue(parameterKeyCode, out object parameter) || !(parameter is T))
+            {
+                return false;
+            }
+            parameterValue = (T)parameter;
+            return true;
+        }
+
+        public static bool TryGetParameter<T>(this ICallInfo info, byte parameterKeyCode, out T parameterValue)
+        {
+            return info.OperationRequest.TryGetParameter(parameterKeyCode, out parameterValue);
         }
 
         #endregion
